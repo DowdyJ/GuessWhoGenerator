3b2aa8239bff7fa13db285fcd4f8272ff76db39d
DowdyJ <joeldowdy12@gmail.com>
2023-10-04 06:02:09 -0700

    Remove duplicate files Update gitignore

--- Program.cs
+++ Program.cs
@@ -4,20 +4,20 @@ using System.Reflection;
 
 namespace GuessWhoGenerator
 {
-    public class Program 
+    public class Program
     {
-        public async static Task Main(string[] args) 
+        public async static Task Main(string[] args)
         {
-            IImageGetter imageGetter = new PicrewImageGetter(@"https://picrew.me/image_maker/516657");
+            IImageGetter imageGetter = new PicrewImageGetter(@"https://picrew.me/en/image_maker/2151243");
             IBoardMaker boardMaker = new StandardBoardMaker();
 
             string outDir = @$"{Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)}/downloadedPictures/";
             int numberOfImagesToGenerate = 10;
             await imageGetter.GetImages(outDir, numberOfImagesToGenerate);
-            
+
             boardMaker.MakeBoard(outDir, "png", outDir, numberOfImagesToGenerate);
 
             return;
         }
-    } 
+    }
 }
